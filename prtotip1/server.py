38711397a33650cbee704ea30f6520b6d8b239a0
Gerardmorsilvestre <gerardmorsilvestre@gmail.com>
2026-01-12 14:50:31 +0100

    xd

--- prtotip1/server.py
+++ prtotip1/server.py
@@ -1,5 +1,54 @@
 from flask import Flask, jsonify, request
 
+class User:
+    def __init__(self,username, nom, password, email, rol="tutor"):
+        self.username=username
+        self.nom=nom
+        self.password=password
+        self.email=email
+        self.rol=rol
+    
+    def __str__(self):
+        return self.nom
+
+#us1=User(username="ama",nom="Rob Halford",password="12345", email="rob@gmail.com",rol="tutor")
+#print(us1)
+users = [
+    User(username="rob",nom="Rob Halford",password="12345", email="rob@gmail.com",rol="tutor"),
+    User(username="john",nom="John Cannigan",password="12345", email="john@gmail.com",rol="tutor"),
+    User(username="maria",nom="Maria Sams",password="12345", email="maria@gmail.com",rol="admin")
+]
+
+class UserDao:
+    def __init__(self):
+        self.users=users
+    
+    def getUserByUsername(self,uname):
+        user = None
+        for u in self.users:
+            if u.username == uname:
+                user = u.__dict__
+        return user
+
+    def addUser(self,u):
+        self.users.append(u)
+        return u
+    
+    def getAllUsers(self):
+        return [user.__dict__ for user in self.users]
+        # Return self users
+
+# Test DAO
+'''user_dao = UserDao()
+response=user_dao.getUserByUsername("maria")
+print(response)
+response=user_dao.getUserByUsername("AAAA")
+print(response) '''
+# End TEST
+
+# Instanciem el Dao User
+user_dao = UserDao()
+
 app = Flask(__name__)
 
 @app.route('/user',methods=['GET'])
@@ -9,14 +58,16 @@ def user():
     username = request.args.get("username",default="")
     # Si els paràmetres OK
     if username != "":
-    # Anar al DAO Server i cercar User per username
-    # respondre amb dades Ususari si trobat
-        resposta="username=" + username
+        # Anar al DAO Server i cercar User per username
+        resposta=user_dao.getUserByUsername(username)
+        # respondre amb dades Ususari si trobat
+        if resposta == None:
+            resposta = {"msg":"Usuari No trobat"}
     else:  #  Si els paràmetres NO ok 
         # respondre error
-        resposta="username No Informat"
+        resposta = {"msg":"Falta paràmetre Username"}
     
-    return resposta
+    return jsonify(resposta)
 
 
 if __name__ == '__main__':
